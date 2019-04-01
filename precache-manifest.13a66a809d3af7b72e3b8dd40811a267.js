b4d15b38eb0d189f66eee8fea71bce49fb67a9b8
Lina ElTohamy <Lina.ElTohamy@itworx.com>
2019-04-01 19:04:05 +0200

    Updates

---
+++ precache-manifest.13a66a809d3af7b72e3b8dd40811a267.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "9bdbe362e67ac97a37cd89b0ae2d05a2",
+    "url": "/ContactList-ReactRouting/static/media/placeholder.9bdbe362.png"
+  },
+  {
+    "revision": "ab927b578798560d791c",
+    "url": "/ContactList-ReactRouting/static/js/runtime~main.0d35618b.js"
+  },
+  {
+    "revision": "9375f186e59e28479574",
+    "url": "/ContactList-ReactRouting/static/js/main.fc4fb53c.chunk.js"
+  },
+  {
+    "revision": "6b1cc443a5e6705396ed",
+    "url": "/ContactList-ReactRouting/static/js/2.28e72181.chunk.js"
+  },
+  {
+    "revision": "9375f186e59e28479574",
+    "url": "/ContactList-ReactRouting/static/css/main.3e770948.chunk.css"
+  },
+  {
+    "revision": "6b1cc443a5e6705396ed",
+    "url": "/ContactList-ReactRouting/static/css/2.b4333a98.chunk.css"
+  },
+  {
+    "revision": "31f5f21fad262ed13211aaa2816c44ed",
+    "url": "/ContactList-ReactRouting/index.html"
+  }
+];
